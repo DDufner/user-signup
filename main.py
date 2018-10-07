4f13dfae2cdce29b2c9f40665d2e88ce3d2f6690
Dominick Dufner <dominickdufner@yahoo.com>
2018-10-07 12:11:11 -0500

    further updates

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import cgi
 import os
 import jinja2 
 
-
 template_dir=os.path.join(os.path.dirname(__file__), 'templates')#checkon this? 
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
@@ -15,53 +14,68 @@ def index():
     template = jinja_env.get_template('user_home.html')
     return template.render()
 
-@app.route('/user_input')
+@app.route('/user_home', methods=['POST'])
 def user_input():
+    no_spaces=check_for_spaces('hell@ oo') #add req.form
+    if not no_spaces:
+        username_error=no_spaces 
+    #repeat for password    
     return inputs.format(username='', username_error='',
     password='', password_error='',
     confirmation='', confirmation_error='',
     email='', email_error='') 
-    no_space=[username, password, verify, email]
 
-def check_for_spaces(no_space):
-    for i in range (len (no_space)):
-        if no_space[i] == ' ':
-            return "cannot contain space(s)" 
-        else: 
-            return True 
+@app.route('/user_home', methods=['POST'])               #before_request #might work? 
+#use nested for loop,  if one has spaces 
+def check_for_spaces(entry):#won't exexut3
+    space_found=False 
+    for i in range (len (entry)):
+        if entry[i] == ' ':
+            space_found=True 
+    if space_found==True:
+        return "cannot contain space(s)" 
+    else:
+        return True          
      
-@app.route('/user_input', methods=['POST'])
+@app.route('/user_home', methods=['POST'])
 def validate_username():
+    # email=valid_email(reqest.form ['email'])
+    # password=reqeust.form['password']
+    # veryify 
+    # if validate_password(validate_password
+    #     #code here.  
     username=request.form['username']
     username_error=''
 
     if len(username) < 3:
-        username_error = "Must be longer than 3 characters"
-    else:
-        if len(username) > 20:
-            username_error="Must be less than 20 characters"
-    else:
-        if username ==""
+        print(username_error = "Must be longer than 3 characters")
+    elif len(username) > 20:
+            print(username_error="Must be less than 20 characters")
+    elif username =="":
             blank_error="Please enter input in field" #? not sure if this will work 
-
+    else:
         if not username_error or blank_error:
-            template=jinja_env.get_template('welcome.html')
-            return template.render(username_error=username_error, username=username)
+            template.render(username_error=username_error, blank_error=blank_error, 
+            username=username)  ## only for errors. 
+            return jinja_env.get_template('welcome.html')
 
-def validate_password():
+def validate_password(password, verify ):
     password=request.form['password']
+    #add lines to execue 
     password_error='' 
-    if len (password<) 3:
+    if len (password) < 3:
         password_error = "Must be longer than 3 characters"
-    else:
-        if len(password) > 20:
+    elif len(password) > 20:
             password_error="Must be less than 20 characters"
-    else:
-        if password ==""
+    elif password =="":
             blank_error="Please enter input in field" #? not sure if this will work 
-            #return True 
-
-def confirm_password():
+    else: 
+        if not password_error or blank_error:
+            template.render(password_error=username_error, blank_error=blank_error, password=password)  ## only for errors. 
+            return jinja_env.get_template('welcome.html')
+            #return True ? 
+    
+def confirm_password(password, verify):
     confirmation=request.form['confirmation']
     confirmation_error=''
     if confirmation !=password:
@@ -74,9 +88,9 @@ def confirm_email(email):
     if email=='':
         return True 
     else: 
-        if i in range (len(email)):
+        for i in range (len(email)):
             i==['a', '.']
-            return True
+            return True 
         else:
             error="Password not valid"
 
