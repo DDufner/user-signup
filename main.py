00ace506607f91a63ffbc26b7fa22d323b4179bd
Dominick Dufner <dominickdufner@yahoo.com>
2018-10-14 15:02:50 -0500

    seperate functions working individually when others commented out but not when multiple active

--- main.py
+++ main.py
@@ -2,100 +2,75 @@ from flask import Flask, request, redirect, render_template
 import cgi
 import os
 import jinja2 
-
-template_dir=os.path.join(os.path.dirname(__file__), 'templates')#checkon this? 
+#try not activing flask ? 
+template_dir=os.path.join(os.path.dirname(__file__), 'templates') 
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask (__name__)
 app.config['DEBUG'] = True
-
-@app.route("/")
+  
+@app.route("/") #default method=GET
 def index():
     template = jinja_env.get_template('user_home.html')
-    return template.render()
+    return template.render() 
 
-@app.route('/user_home', methods=['POST'])
-def user_input():
-    no_spaces=check_for_spaces('hell@ oo') #add req.form
-    if not no_spaces:
-        username_error=no_spaces 
-    #repeat for password    
-    return inputs.format(username='', username_error='',
-    password='', password_error='',
-    confirmation='', confirmation_error='',
-    email='', email_error='') 
-
-@app.route('/user_home', methods=['POST'])               #before_request #might work? 
-#use nested for loop,  if one has spaces 
-def check_for_spaces(entry):#won't exexut3
-    space_found=False 
-    for i in range (len (entry)):
-        if entry[i] == ' ':
-            space_found=True 
-    if space_found==True:
-        return "cannot contain space(s)" 
-    else:
-        return True          
-     
-@app.route('/user_home', methods=['POST'])
-def validate_username():
-    # email=valid_email(reqest.form ['email'])
-    # password=reqeust.form['password']
-    # veryify 
-    # if validate_password(validate_password
-    #     #code here.  
+@app.route('/', methods=['POST']) # aded 'userhome #post actually checks validations
+def user_input(): 
     username=request.form['username']
     username_error=''
-
-    if len(username) < 3:
-        print(username_error = "Must be longer than 3 characters")
+    password_error=''
+    confirmation_error=''
+    template = jinja_env.get_template('user_home.html')
+    welcome_template = jinja_env.get_template('welcome.html')
+    if len(username) < 3: 
+        username_error = "Must be greater than 3 characters"
+        return template.render(username_error=username_error, username=username) 
     elif len(username) > 20:
-            print(username_error="Must be less than 20 characters")
-    elif username =="":
-            blank_error="Please enter input in field" #? not sure if this will work 
+        username_error = "Must be less than 20 characters"
+        return template.render(username_error=username_error, username=username) 
     else:
-        if not username_error or blank_error:
-            template.render(username_error=username_error, blank_error=blank_error, 
-            username=username)  ## only for errors. 
-            return jinja_env.get_template('welcome.html')
+        return welcome_template.render()
 
-def validate_password(password, verify ):
+def password_input(): 
     password=request.form['password']
-    #add lines to execue 
-    password_error='' 
-    if len (password) < 3:
-        password_error = "Must be longer than 3 characters"
+    #username=''
+    password_error=''
+    template = jinja_env.get_template('user_home.html')
+    welcome_template = jinja_env.get_template('welcome.html')
+    if len(password) < 3: 
+        password_error = "Must be greater than 3 characters"
+        return template.render(password_error=password_error, password=password) 
     elif len(password) > 20:
-            password_error="Must be less than 20 characters"
-    elif password =="":
-            blank_error="Please enter input in field" #? not sure if this will work 
-    else: 
-        if not password_error or blank_error:
-            template.render(password_error=username_error, blank_error=blank_error, password=password)  ## only for errors. 
-            return jinja_env.get_template('welcome.html')
-            #return True ? 
-    
-def confirm_password(password, verify):
+        password_error = "Must be less than 20 characters"
+        return template.render(password_error=password_error, password=password) 
+    else:
+        return welcome_template.render()
+
+def confirm_password():
+    password=request.form['password']
     confirmation=request.form['confirmation']
     confirmation_error=''
-    if confirmation !=password:
-        confirmation_error="Passwords do no match"
-    else:
-        return True 
-
-def confirm_email(email):
-    email=request.form['email']
-    if email=='':
-        return True 
+    template = jinja_env.get_template('user_home.html')
+    welcome_template = jinja_env.get_template('welcome.html')
+    if not password==confirmation:
+        confirmation_error ="Password confirmation does not match password"
+        return template.render(confirmation_error=confirmation_error, confirmation=confirmation)
     else: 
-        for i in range (len(email)):
-            i==['a', '.']
-            return True 
-        else:
-            error="Password not valid"
+        return welcome_template.render()
 
-@app.route('/user_input')
-def valid_username():
-    return template.render('welcome.html')
+def is_email():     
+    email=request.form['email']  
+    email_error='' 
+    template = jinja_env.get_template('user_home.html')
+    welcome_template = jinja_env.get_template('welcome.html')
+    atsign_index = email.find('@')
+    atsign_present = atsign_index >=0
+    if not atsign_present:
+        email_error="Email is not valid"
+        return template.render(email_error=email_error, email=email)
+    else:
+        domain_dot_index = email.find('.', atsign_index)
+        domain_dot_present = domain_dot_index >=0
+        return welcome_template.render()
 
 app.run()
