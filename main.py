c6b4bbee1f45fc985651bd999c4c380daed1bd1d
Dominick Dufner <dominickdufner@yahoo.com>
2018-10-15 22:23:47 -0500

    final code

--- main.py
+++ main.py
@@ -17,9 +17,17 @@ def index():
 @app.route('/', methods=['POST']) # aded 'userhome #post actually checks validations
 def user_input(): 
     username=request.form['username']
+    password=request.form['password'] #
+    confirmation=request.form['confirmation'] #
+    email=request.form['email']  
+    email_error='' 
     username_error=''
     password_error=''
     confirmation_error=''
+    atsign_index = email.find('@')
+    atsign_present = atsign_index >=0
+    domain_dot_index = email.find('.', atsign_index)
+    domain_dot_present = domain_dot_index >=0
     template = jinja_env.get_template('user_home.html')
     welcome_template = jinja_env.get_template('welcome.html')
     if len(username) < 3: 
@@ -28,49 +36,24 @@ def user_input():
     elif len(username) > 20:
         username_error = "Must be less than 20 characters"
         return template.render(username_error=username_error, username=username) 
-    else:
-        return welcome_template.render()
-
-def password_input(): 
-    password=request.form['password']
-    #username=''
-    password_error=''
-    template = jinja_env.get_template('user_home.html')
-    welcome_template = jinja_env.get_template('welcome.html')
-    if len(password) < 3: 
+    elif len(password) < 3: 
         password_error = "Must be greater than 3 characters"
         return template.render(password_error=password_error, password=password) 
     elif len(password) > 20:
         password_error = "Must be less than 20 characters"
-        return template.render(password_error=password_error, password=password) 
-    else:
-        return welcome_template.render()
-
-def confirm_password():
-    password=request.form['password']
-    confirmation=request.form['confirmation']
-    confirmation_error=''
-    template = jinja_env.get_template('user_home.html')
-    welcome_template = jinja_env.get_template('welcome.html')
-    if not password==confirmation:
+        return template.render(password_error=password_error, password=password)   
+    elif not password==confirmation:
         confirmation_error ="Password confirmation does not match password"
         return template.render(confirmation_error=confirmation_error, confirmation=confirmation)
-    else: 
+    elif email=='':  # how to ignore if no email present 
         return welcome_template.render()
-
-def is_email():     
-    email=request.form['email']  
-    email_error='' 
-    template = jinja_env.get_template('user_home.html')
-    welcome_template = jinja_env.get_template('welcome.html')
-    atsign_index = email.find('@')
-    atsign_present = atsign_index >=0
-    if not atsign_present:
+    elif not atsign_present:
+        email_error="Email is not valid"
+        return template.render(email_error=email_error, email=email)
+    elif not domain_dot_present:
         email_error="Email is not valid"
         return template.render(email_error=email_error, email=email)
     else:
-        domain_dot_index = email.find('.', atsign_index)
-        domain_dot_present = domain_dot_index >=0
-        return welcome_template.render()
+        return welcome_template.render(username=username)
 
 app.run()
